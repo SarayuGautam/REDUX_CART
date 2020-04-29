1adc46bdf76f1008a8c15cf79b76ea48ded659d2
Sarayu Gautam <sarayugautam1@gmail.com>
2020-04-29 09:40:18 +0545

    Added Cart-items Changes to be committed: 	modified:   src/cart-items.js

--- src/cart-items.js
+++ src/cart-items.js
@@ -1,26 +1,42 @@
 export default [
   {
     id: 1,
-    title: "Samsung Galaxy S7",
-    price: 599.99,
+    title: "ASUS Vivobook S15",
+    price: 1100,
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
-    amount: 1
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT9HPShoBBn6u0AlZZIs0YeslZx_L-0LwDTqsTqjTz_EAq7Kf1n&usqp=CAU",
+    amount: 1,
   },
   {
     id: 2,
-    title: "google pixel ",
-    price: 499.99,
+    title: "Dell XPS 13",
+    price: 1415,
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
-    amount: 1
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSWqRODmZxotTzYwFVJD-s9Rt-lpgbzfJjwL-dZj0acISyNRvyl&usqp=CAU",
+    amount: 1,
   },
   {
     id: 3,
-    title: "Xiaomi Redmi Note 2",
-    price: 699.99,
+    title: "HP stream 14",
+    price: 2200,
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
-    amount: 1
-  }
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDCXvbOQL9rtS4QOd1_NaIPfMiVvl9P0-ho3dn0gT4IUIKrsoy&usqp=CAU",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Apple mackbook air",
+    price: 999,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQFROcCbxlqUqMV_BAMnPCFXCZh7dCFe43rRl1pGSx457AIuVrM&usqp=CAU",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Microsoft Surface book 2",
+    price: 1499,
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSvuiIT3eFbxQGsYZ5LCkCTPy8fTNpJwsC6jvhQJZgbnbN61qHj&usqp=CAU",
+    amount: 1,
+  },
 ];
