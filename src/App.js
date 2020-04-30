1ed8721ef8fc1ef4baef7c71461a08de139ee32b
Sarayu Gautam <sarayugautam1@gmail.com>
2020-04-30 09:28:09 +0545

    Project Refactored Changes to be committed: 	modified:   src/App.js 	modified:   src/actions.js 	modified:   src/components/CartContainer.js 	modified:   src/components/CartItem.js 	modified:   src/reducer.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
-import cartItems from "./cart-items";
 import { createStore } from "redux";
 import { reducer } from "./reducer";
 // redux stuff
 import { Provider } from "react-redux";
-const initStore = {
-  cart: cartItems,
-  total: 100,
-  amount: 5,
-};
 
-const store = createStore(reducer, initStore);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   // cart setup
--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,19 @@
-import { INCREASE, DECREASE, CLEAR_CART, REMOVE, GET_TOTALS } from "./actions";
+import {
+  INCREASE,
+  DECREASE,
+  CLEAR_CART,
+  REMOVE,
+  GET_TOTALS,
+  TOGGLE_AMOUNT,
+} from "./actions";
+import cartItems from "./cart-items";
 
-export function reducer(state, action) {
-  // if (action.type === CLEAR_CART) {
-  //   return { ...state, cart: [] };
-  // }
-  // return state;
+const initStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+export function reducer(state = initStore, action) {
   switch (action.type) {
     case CLEAR_CART:
       return { ...state, cart: [] };
@@ -18,19 +27,13 @@ export function reducer(state, action) {
       };
 
     case DECREASE:
-      let tempCart = [];
-      if (action.payload.amount === 1) {
-        tempCart = state.cart.filter(
-          (cartItem) => cartItem.id !== action.payload.id
-        );
-      } else {
-        tempCart = state.cart.map((cartItem) => {
-          if (cartItem.id === action.payload.id) {
-            cartItem = { ...cartItem, amount: cartItem.amount - 1 };
-          }
-          return cartItem;
-        });
-      }
+      let tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+
       return { ...state, cart: tempCart };
 
     case INCREASE:
@@ -46,17 +49,35 @@ export function reducer(state, action) {
       };
     case GET_TOTALS:
       let { total, amount } = state.cart.reduce(
-        (cardTotal, cartItem) => {
+        (cartTotal, cartItem) => {
           const { price, amount } = cartItem;
-          cardTotal.amount += amount;
-          return cardTotal;
+          const itemTotal = price * amount;
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+          return cartTotal;
         },
         {
           total: 0,
           amount: 0,
         }
       );
+      total = parseFloat(total.toFixed(2));
       return { ...state, total, amount };
+    case TOGGLE_AMOUNT:
+      return {
+        ...state,
+        cart: state.cart.map((cartItem) => {
+          if (cartItem.id === action.payload.id) {
+            if (action.payload.toggle === "inc") {
+              return (cartItem = { ...cartItem, amount: cartItem.amount + 1 });
+            }
+            if (action.payload.toggle === "dec") {
+              return (cartItem = { ...cartItem, amount: cartItem.amount - 1 });
+            }
+          }
+          return cartItem;
+        }),
+      };
     default:
       return state;
   }
